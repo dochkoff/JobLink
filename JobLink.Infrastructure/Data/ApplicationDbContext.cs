2e925a6ad0260f651d5d4e6547ccd06e60be3b41
Dochkoff <dochkoff@gmail.com>
2024-04-01 00:01:35 +0200

    Moved DbContext and removed old migrations

--- JobLink.Infrastructure/Data/ApplicationDbContext.cs
+++ JobLink.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace JobLink.Data
+namespace JobLink.Infrastructure.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
--- JobLink/Program.cs
+++ JobLink/Program.cs
@@ -1,4 +1,4 @@
-using JobLink.Data;
+using JobLink.Infrastructure.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
