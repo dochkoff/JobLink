a6f931c599c238733d245e068a229ac996eedc01
Dochkoff <Dochkoff@DESKTOP-8JG38B6>
2024-03-31 21:52:57 +0200

    Add project files.

---
+++ JobLink/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JobLink.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
