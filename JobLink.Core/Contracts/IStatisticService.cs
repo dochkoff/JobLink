d71d708f3a6067484b34c4b8fd5632a36b53d919
Dochkoff <dochkoff@gmail.com>
2024-04-03 19:54:46 +0200

    The Statistics Service is created

---
+++ JobLink.Core/Contracts/IStatisticService.cs
@@ -0,0 +1,9 @@
+﻿using JobLink.Core.Models.Statistics;
+
+namespace JobLink.Core.Contracts
+{
+    public interface IStatisticService
+    {
+        Task<StatisticServiceModel> TotalAsync();
+    }
+}
---
+++ JobLink.Core/Services/StatisticService.cs
@@ -0,0 +1,33 @@
+﻿using JobLink.Core.Contracts;
+using JobLink.Core.Models.Statistics;
+using JobLink.Infrastructure.Data.Common;
+using JobLink.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobLink.Core.Services
+{
+    public class StatisticService : IStatisticService
+    {
+        private readonly IRepository repository;
+
+        public StatisticService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+
+        public async Task<StatisticServiceModel> TotalAsync()
+        {
+            int totalJobs = await repository.AllReadOnly<Job>()
+                .CountAsync();
+
+            int totalEmployers = await repository.AllReadOnly<Employer>()
+                .CountAsync();
+
+            return new StatisticServiceModel()
+            {
+                TotalJobs = totalJobs,
+                TotalEmployers = totalEmployers
+            };
+        }
+    }
+}
