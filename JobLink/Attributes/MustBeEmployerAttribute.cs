5009c849ff37c67dfbd5dfff055ec3ff22fd46d8
Dochkoff <dochkoff@gmail.com>
2024-04-03 16:57:05 +0200

    Attributes added

---
+++ JobLink/Attributes/MustBeEmployerAttribute.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using JobLink.Core.Contracts;
+using JobLink.Controllers;
+
+namespace JobLink.Attributes
+{
+    public class MustBeEmployerAttribute : ActionFilterAttribute
+    {
+        //public override void OnActionExecuting(ActionExecutingContext context)
+        //{
+        //    base.OnActionExecuting(context);
+
+        //    IEmployerService? employerService = context.HttpContext.RequestServices.GetService<IEmployerService>();
+
+        //    if (employerService == null)
+        //    {
+        //        context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+        //    }
+
+        //    if (employerService != null
+        //        && employerService.ExistsByIdAsync(context.HttpContext.User.Id()).Result == false)
+        //    {
+        //        context.Result = new RedirectToActionResult(nameof(EmployerController.Become), "Employer", null);
+        //    }
+        //}
+    }
+}
---
+++ JobLink/Attributes/NotAnEmployerAttribute.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using JobLink.Core.Contracts;
+
+namespace JobLink.Attributes
+{
+    public class NotAnEmployerAttribute : ActionFilterAttribute
+    {
+        //public override void OnActionExecuting(ActionExecutingContext context)
+        //{
+        //    base.OnActionExecuting(context);
+
+        //    IEmployerService? agentService = context.HttpContext.RequestServices.GetService<IEmployerService>();
+
+        //    if (agentService == null)
+        //    {
+        //        context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+        //    }
+
+        //    if (agentService != null
+        //        && agentService.ExistsByIdAsync(context.HttpContext.User.Id()).Result)
+        //    {
+        //        context.Result = new StatusCodeResult(StatusCodes.Status400BadRequest);
+        //    }
+        //}
+    }
+}
