43e88b8842016cf8ddb69ff5e6d91ff9b0b30aac
Pavel Dochkov <hounds.07cinder@icloud.com>
2024-04-09 01:20:26 +0300

    Removed Initial Error View And Model

--- JobLink/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace JobLink.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
