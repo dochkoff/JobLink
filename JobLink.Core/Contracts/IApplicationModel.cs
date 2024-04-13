b8d2c7825cdb51f00d86991ef39f0e309902e7db
Pavel Dochkov <hounds.07cinder@icloud.com>
2024-04-13 12:13:16 +0300

    Added Application View Model

---
+++ JobLink.Core/Contracts/IApplicationModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobLink.Core.Contracts
+{
+    internal interface IApplicationModel
+    {
+        public int Id { get; set; }
+
+        public string DateAndTime { get; set; }
+    }
+}
---
+++ JobLink.Core/Models/Application/ApplicationDetailsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using JobLink.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobLink.Core.Models.Application
+{
+    internal class ApplicationDetailsViewModel : IApplicationModel
+    {
+        public int Id { get; set;}
+
+        public string DateAndTime { get; set;} = string.Empty;
+
+        public string ApplicantName { get; set; } = string.Empty;
+
+        public string ApplicantPhoneNumber { get; set; } = string.Empty;
+
+        public string ApplicantResumeUrl { get; set; } = string.Empty;
+    }
+}
