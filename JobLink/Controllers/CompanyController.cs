147034ab1f98c893c9eeb08e7029c43804d37432
Pavel Dochkov <hounds.07cinder@icloud.com>
2024-04-14 23:28:43 +0300

    Added Add Company Functionality

--- JobLink/Controllers/CompanyController.cs
+++ JobLink/Controllers/CompanyController.cs
@@ -1,12 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using JobLink.Core.Contracts;
+using JobLink.Core.Extensions;
+using JobLink.Core.Models.Company;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JobLink.Controllers
 {
     public class CompanyController : BaseController
     {
-        public IActionResult Index()
+
+        private readonly ICompanyService companyService;
+
+        public CompanyController(ICompanyService _companyService)
         {
-            return View();
+            companyService = _companyService;
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+             var model = new CompanyFormModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(CompanyFormModel model)
+        {
+            string newCompanyId = await companyService.CreateCompanyAsync(model);
+
+            return RedirectToAction(nameof(Details), new { companyId = newCompanyId, information = model.GetCompanyInformation() });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(string companyId, string information)
+        {
+
+
+            var model = await companyService.CompanyDetailsByIdAsync(companyId);
+
+            if (information != model.GetCompanyInformation())
+            {
+                return NotFound();
+            }
+
+            return View(model);
+        }
+
     }
 }
