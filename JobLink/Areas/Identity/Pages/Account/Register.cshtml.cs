b26efcb6e733e4489de94e3dc497a193230a2ced
Pavel Dochkov <hounds.07cinder@icloud.com>
2024-04-16 00:55:38 +0300

    User Full Name Is Displayed From User Claim

--- JobLink/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ JobLink/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -2,23 +2,16 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Text.Encodings.Web;
-using System.Threading;
-using System.Threading.Tasks;
 using JobLink.Infrastructure.Data.Models;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
-using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Text.Encodings.Web;
 using static JobLink.Infrastructure.Constants.DataConstants;
 
 namespace JobLink.Areas.Identity.Pages.Account
@@ -135,6 +128,8 @@ namespace JobLink.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
+
+                    await _userManager.AddClaimAsync(user, new System.Security.Claims.Claim("user:fullname", $"{user.FirstName} {user.LastName}"));
 
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
