7bcce7df6fc04b4187543afea2d75be17491d360
Pavel Dochkov <hounds.07cinder@icloud.com>
2024-05-11 13:44:11 +0300

    See all job posts from Admin Center

---
+++ JobLink/Areas/Admin/Controllers/JobController.cs
@@ -0,0 +1,34 @@
+﻿using JobLink.Core.Contracts;
+using JobLink.Core.Models.Job;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobLink.Areas.Admin.Controllers
+{
+    public class JobController : AdminBaseController
+    {
+        private readonly IJobService jobService;
+
+        public JobController(
+            IJobService _jobService)
+        {
+            jobService = _jobService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AllJobPosts([FromQuery] AllJobsQueryModel model)
+        {
+            var jobs = await jobService.AllAsync(
+                model.JobCategory,
+                model.SearchTerm,
+                model.Sorting,
+                model.CurrentPage,
+                model.JobsPerPage);
+
+            model.TotalJobsCount = jobs.TotalJobsCount;
+            model.Jobs = jobs.Jobs;
+            model.JobCategories = await jobService.AllCategoriesNamesAsync();
+
+            return View(model);
+        }
+    }
+}
